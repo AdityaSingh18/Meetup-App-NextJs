4b7f749eccb0ab5334c055c8212346e1a4d06c28
Aditya <aditya18.mail@gmail.com>
2023-05-30 12:15:22 +0530

    _app.js & Layout Wrapper

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import MeetupList from '../components/meetups/MeetupList';
+import Layout from '../components/layout/Layout'
 
 
 const DUMMY_MEETUPS = [
