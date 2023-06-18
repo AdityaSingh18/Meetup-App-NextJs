1214edbccd085e822d4ef610fc9e2cbf72b6d202
Aditya <aditya18.mail@gmail.com>
2023-06-18 13:44:03 +0530

    getStaticPaths & Fallback Pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,5 +10,27 @@ function MeetupDetails() {
     />
   );
 }
+export function getStaticPaths() {
+  return {
+    fallback:false,
+    paths: [{ params: { meetupId: "m1" } },
+    { params: { meetupId: "m2" } }],
+  };
+}
 
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://thumbs.dreamstime.com/b/beautiful-shot-architectural-budling-london-191060517.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "some street,some City",
+        description: "this is first meetup",
+      },
+    },
+  };
+}
 export default MeetupDetails;
